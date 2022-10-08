c9a95af71ab5cc5c37bec6c6e82a45ea420c98f7
robinNcode <msmrobin518@gmail.com>
2022-10-08 19:30:03 +0600

    login ui app added

---
+++ loginUI/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/auth/Login'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
